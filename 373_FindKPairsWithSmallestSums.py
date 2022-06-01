59190ea8f1414e386c1ec9d8ef6a86c39907ea5c
yudaiOfRiver <ykawa412@gmail.com>
2022-06-01 10:40:42 +0900

    renew 373

--- 373_FindKPairsWithSmallestSums.py
+++ 373_FindKPairsWithSmallestSums.py
@@ -1,10 +1,12 @@
+import heapq
+
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
         myHeap = []
-        
+
         for i in range(len(nums1)):
             heapq.heappush(myHeap,(nums1[i] + nums2[0], i, 0))
-            
+
         res = []
         #print(myHeap)
         while k > 0 and myHeap:
@@ -12,6 +14,6 @@ class Solution:
             res.append([nums1[i],nums2[j]])
             if j+1 < len(nums2):
                 heapq.heappush(myHeap, (nums1[i] + nums2[j+1], i, j+1))
-                print([i, j+1])
+                #print([i, j+1])
             k -= 1
-        return res        
+        return res
